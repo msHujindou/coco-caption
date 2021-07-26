e768f5ae404c928d084f209e12087df4cd6c75c9
Hujindou <v-jindhu@microsoft.com>
2021-07-26 19:23:21 +0800

    add necessary file for calculating CIDEr & Spice for User Inserted Clean

---
+++ convert_tsv_results_to_coco.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import json
+
+
+def extract_tsv_prediction_results_to_pdf(file_name):
+    pdf = pd.read_csv(file_name, header=None, names=["img_key", "pred"], delimiter="\t")
+    extracted = pdf.apply(lambda x: json.loads(x["pred"]), axis=1, result_type="expand")
+    pdf["results"] = extracted.apply(lambda x: x["description"]["captions"], axis=1)
+    return pdf, extracted
+
+
+def export_pdf_to_json(pdf):
+    resjson = []
+    for idx, row in pdf.iterrows():
+        for itm in row["results"]:
+            resjson.append({"image_id": row["img_key"], "caption": itm["text"]})
+        pass
+    with open("office.json", "w") as fw:
+        json.dump(resjson, fw)
+        pass
+    return resjson
+
+
+df, _ = extract_tsv_prediction_results_to_pdf(
+    "user_inserted_clean.v3.2-nocl.20210507.tsv"
+)
+js = export_pdf_to_json(df)
+print(js)
